bce6b251e19fbaac9f2add0c0f1c69d232917d4a
jkohl7 <jkohl7@my.wctc.edu>
2020-02-05 11:32:55 -0600

    update

--- src/edu/wctc/RoomWriter.java
+++ src/edu/wctc/RoomWriter.java
@@ -1,9 +1,19 @@
 package edu.wctc;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.io.Serializable;
 
 public class RoomWriter {
-    public void writeRoomFile( String fileName, ArrayList<Room> roomList ) {  }
+    public void writeRoomFile( String fileName, ArrayList<Room> roomList ) throws IOException {
+        FileOutputStream fos = new FileOutputStream(fileName);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+
+
+
+    }
 
 }
